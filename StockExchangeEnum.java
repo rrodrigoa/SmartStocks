a7b9d204af487267928231e6a7c54eef38054be5
Rodrigo Alves <rrodrigoa@gmail.com>
2014-06-06 10:51:54 -0700

    Create StockExchangeEnum.java

---
+++ StockExchangeEnum.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stockanalysis;
+
+/**
+ *
+ * @author rodrigoa
+ */
+public enum StockExchangeEnum {
+	NASDAQ,
+	NYSE,
+	LON,
+	SWX,
+        INDEXES
+}
+
