6401de84d9f683560913a00eeac675ac8f7cee27
Rodrigo Alves <rrodrigoa@gmail.com>
2014-06-06 10:52:06 -0700

    Create StockInfo.java

---
+++ StockInfo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stockanalysis;
+
+/**
+ *
+ * @author rodrigoa
+ */
+import java.util.Date;
+
+
+public class StockInfo {
+	public Date date;
+	public float open;
+	public float high;
+	public float low;
+	public float close;
+	public long volume;
+}
+
