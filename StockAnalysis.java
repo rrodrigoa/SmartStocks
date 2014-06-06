efbc43973be3719ce8d402eb84e3c857c6336d5e
Rodrigo Alves <rrodrigoa@gmail.com>
2014-06-06 10:51:05 -0700

    Create StockAnalysis.java

---
+++ StockAnalysis.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stockanalysis;
+
+/**
+ *
+ * @author rodrigoa
+ */
+public class StockAnalysis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.startSelectStockExchangePanel();
+    }
+}
